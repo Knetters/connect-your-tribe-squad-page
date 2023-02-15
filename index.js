175e5e82cb6354e69045835410a0d89185decd61
ThomasScheepers <thomas@scheepers.com>
2023-02-15 13:18:49 +0100

    Nu werkt het wesley kaas

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ import fetch from "node-fetch";
 // Create a new Express app
 const app = express();
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/squad-a-2022?orderBy=name&direction=ASC'
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error)
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
 
 // Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
@@ -21,7 +18,13 @@ app.use(express.static("public"));
 app.get('/', function (request, response) {
   console.log(request.query.squad)
 
-  response.render('index', data)
+  let slug = request.query.squad || 'squad-a-2022'
+  let orderBy = request.query.orderBy || 'name' + '&direction=ASC'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+
+  fetchJson(squadUrl).then((data) => {
+    response.render('index', data)
+  })
 });
 
 // Set the port number and start the server
@@ -30,3 +33,13 @@ app.listen(port, () => {
   console.log(`Application available on: http://localhost:${port}`);
 });
 
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
