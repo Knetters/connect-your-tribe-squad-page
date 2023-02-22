8057266ae8c570c6df71e70a4096c5dc722bda0d
ThomasScheepers <thomas@scheepers.com>
2023-02-22 10:34:58 +0100

    Laatste versie

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const url = 'https://whois.fdnd.nl/api/v1/squad/'
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // Serve static files from the public directory
 app.use(express.static("public"));
 
@@ -26,13 +29,34 @@ app.get('/', function (request, response) {
   })
 });
 
+app.post('/', function (request, response) {
+  console.log(request.body)
+  const payload = {
+    id: "",
+    like: ""
+  }
+  const headers = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(request.body),
+  }
+  const url = 'https://whois.fdnd.nl/api/v1/shout'
+
+  fetchJson(url, headers).then((data) => {
+    response.redirect(303, '/')
+  })
+})
+
 // Set the port number and start the server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Application available on: http://localhost:${port}`);
 });
 
-async function fetchJson(url) {
+async function fetchJson(url, payload) {
   return await fetch(url)
     .then((response) => response.json())
     .catch((error) => error)
--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const likeBtns = document.querySelectorAll('.like-btn');
 likeBtns.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     // toggle the 'like' and 'fill-heart' classes on the clicked button
-    btn.classList.toggle('like');
+    btn.classList.toggle('on');
     btn.classList.toggle('fill-heart');
 
     // add a unique animation class based on the button's index
